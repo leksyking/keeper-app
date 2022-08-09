f15a2c6557bd551b6fbd1faf3d9ea494ad29e6bb
Leksyking <gbemilekeogundipe@gmail.com>
2022-08-08 23:06:01 -0700

    map notes in notes.js and render in the app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react"
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
+const AllNotes = notes.map(note => <Note 
+    key={note.key}
+    title={note.title}
+    content={note.content}
+/>)
+
 
 function App(){
     return (
         <div>
             <Header />
-            <Note />
+            {AllNotes}
             <Footer /> 
         </div>
     )
